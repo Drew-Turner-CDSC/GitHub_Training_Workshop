a53306d59aced334a3a678009121231d87a86164
Drew-Turner-CDSC <127952705+Drew-Turner-CDSC@users.noreply.github.com>
2026-01-08 16:02:11 +0000

    Simplified package requirements to only need tidyverse

--- clean_analyze_outbreak_dplyr.R
+++ clean_analyze_outbreak_dplyr.R
@@ -5,12 +5,7 @@
 
 # ---- Load packages ----
 
-  library(readr)
-  library(dplyr)
-  library(tidyr)
-  library(stringr)
-  library(lubridate)
-  library(ggplot2)
+  library(tidyverse)
 
 
 # ---- Read data ----
